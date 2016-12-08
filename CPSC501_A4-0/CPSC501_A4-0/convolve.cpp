c667174a300474afb4d078706f11c9644deb42da
vektor <vektor@VEKTOR-PC>
2016-12-08 01:15:19 -0700

    Programming plan

--- CPSC501_A4-0/CPSC501_A4-0/convolve.cpp
+++ CPSC501_A4-0/CPSC501_A4-0/convolve.cpp
@@ -4,6 +4,13 @@
 
 int main()
 {
+
+	// Programming Plan:
+		// 0. Create WAV file struct, include required headers.
+	// 1. Define function prototypes. Each of these will be units for regression testing.
+	// 2. Refactor function prototypes into external header file, time pending.
+	// 3. Test convolution using Visual Studio facilities; check if proper WAV file outputting.
+	// 4. Merge final product onto master, removing all auto-generated stubs.
     return 0;
 }
 
